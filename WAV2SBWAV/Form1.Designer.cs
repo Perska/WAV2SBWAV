64c0a7da77df0f9a60d4c75d8a4cb7e7ac6966e1
Perska <Perska@users.noreply.github.com>
2016-03-10 18:05:45 +0200

    Added files via upload

---
+++ WAV2SBWAV/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WAV2SBWAV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.maskedTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.ForeColor = System.Drawing.Color.Red;
+            this.maskedTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.ReadOnly = true;
+            this.maskedTextBox1.Size = new System.Drawing.Size(400, 17);
+            this.maskedTextBox1.TabIndex = 0;
+            this.maskedTextBox1.Text = "MAKE SURE THE BPS IS 8 AND THE SAMPLE RATE 8180!";
+            this.maskedTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(140, 92);
+            this.progressBar1.MarqueeAnimationSpeed = 1;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(272, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(400, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load WAV";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(400, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save SBWAV";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "wav";
+            this.openFileDialog1.Filter = "Waveform|*.wav";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 93);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(122, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Ignore sample count";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 122);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Name = "Form1";
+            this.Text = "WAV 2 SBWAV";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ WAV2SBWAV/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace WAV2SBWAV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class data
+        {
+            public static byte[] input;
+            public static byte[] output = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Oops?
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                WaveFileReader tmp = new WaveFileReader(openFileDialog1.FileName);
+                if ((tmp.WaveFormat.SampleRate == 8180) || checkBox1.Checked)
+                {
+                    if (tmp.WaveFormat.BitsPerSample == 8)
+                    {
+                        if (tmp.SampleCount <= 245400 || checkBox1.Checked)
+                        {
+                            data.input = new byte[tmp.Length];
+                            tmp.Read(data.input, 0, Convert.ToInt32(tmp.Length - 1));
+                            data.output = new byte[(tmp.Length*4)+128];
+                            setbinaryvalues(ref data.output, 0, "00 00 01 00 00 00 00 00 00 00 00 00 00 D7 01 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 50 43 42 4E 30 30 30 31 04 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"); //DAT flag, date
+                            setbinaryvalues(ref data.output, 92, (tmp.Length % 256).ToString("X2"));
+                            setbinaryvalues(ref data.output, 93, ((tmp.Length >> 8) % 256).ToString("X2"));
+                            setbinaryvalues(ref data.output, 94, ((tmp.Length >> 16) % 256).ToString("X2"));
+                            setbinaryvalues(ref data.output, 95, ((tmp.Length >> 24) % 256).ToString("X2"));
+                            setbinaryvalues(ref data.output, 108, (Convert.ToInt32(tmp.Length / 8180.0f * 60.0f) % 256).ToString("X2"));
+                            setbinaryvalues(ref data.output, 109, (((Convert.ToInt32(tmp.Length / 8180.0f * 60.0f) >> 8) % 256)).ToString("X2"));
+                            setbinaryvalues(ref data.output, 110, (((Convert.ToInt32(tmp.Length / 8180.0f * 60.0f) >> 16) % 256)).ToString("X2"));
+                            setbinaryvalues(ref data.output, 111, (((Convert.ToInt32(tmp.Length / 8180.0f * 60.0f) >> 24) % 256)).ToString("X2"));
+                            setbinaryvalues(ref data.output, data.output.Length - 19, "57 41 56 32 53 42 57 41 56 20 46 6F 6F 74 65 72 00 00");
+                            progressBar1.Maximum = data.input.Length;
+                            for (int i = 0; i < data.input.Length; i++)
+                            {
+                                progressBar1.Value = i+1;
+                                setbinaryvalues(ref data.output, 112+i*4, data.input[i].ToString("X2"));
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("ERROR: Too many samples.\nSplit your audio file?");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("ERROR: The amount of bits per sample is not 8.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("ERROR: Sample rate is not 8180.");
+                }
+            }
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (data.output == null)
+            {
+                MessageBox.Show("ERROR: NO DATA HAS BEEN WRITTEN\nDid you really load a WAV file?");
+            }
+            else
+            {
+                DialogResult res = saveFileDialog1.ShowDialog();
+                if (res==DialogResult.OK)
+                {
+                    System.IO.File.WriteAllBytes(saveFileDialog1.FileName,data.output);
+                }
+            }
+        }
+
+        private void setbinaryvalues(ref byte[] temporary, int start, string data)
+        {
+            string[] datas = data.Split(' ');
+            for (int i = 0; i < datas.Length; i++)
+            {
+                temporary[start + i] = Convert.ToByte(datas[i],16);
+            }
+        }
+    }
+}
